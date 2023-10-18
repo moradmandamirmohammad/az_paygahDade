c9362dcf94dab08fe04c6aba595282e35d5e9aa0
moradmandamirmohammad <113003571+moradmandamirmohammad@users.noreply.github.com>
2023-10-18 10:35:28 +0330

    assignment 2

---
+++ tamrin/s2/users_db sql.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (user_id, user_fullName, user_phoneNumber, user_email, user_password, user_phoneVerification, user_emailVerification)
+VALUES ('1', 'amir mohammad moradmand', '09123456789', 'amir@gmail.com', '123456789', null, null);
